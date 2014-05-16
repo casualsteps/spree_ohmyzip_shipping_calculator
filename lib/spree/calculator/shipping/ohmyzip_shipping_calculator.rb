590de98f323072b38fdf997f1316826922462365
James Duffy <jamesmartinduffy@gmail.com>
2014-05-16 15:57:09 +0900

    Got spec test working without errors

--- lib/spree/calculator/shipping/ohmyzip_shipping_calculator.rb
+++ lib/spree/calculator/shipping/ohmyzip_shipping_calculator.rb
@@ -1,4 +1,4 @@
-class Spree::Calculator::Ohmyzip < Spree::ShippingCalculator
+class Spree::Calculator::Shipping::Ohmyzip < Spree::ShippingCalculator
   preference :default_weight, :decimal, default: 1
 
   def self.description
@@ -16,22 +16,21 @@ class Spree::Calculator::Ohmyzip < Spree::ShippingCalculator
   def compute_package(package)
     content_items = package.contents
     total_weight = total_weight(content_items)
-    base_price = order_contains_ilbantongwan? ? 7.50 : 6.50
+    base_price = order_contains_ilbantongwan?(package) ? 7.50 : 6.50
 
     shipping_cost = total_weight*2 + base_price
     shipping_cost
   end
 
-  private
   def total_weight(contents)
     weight = 0
     contents.each do |item|          
       weight += item.quantity * (item.variant.weight > 0.0 ? item.variant.weight : preferred_default_weight)
     end
-    
     weight
   end
 
+  private
   def order_contains_ilbantongwan?(contents)
     return false
     ### TODO: set up the below logic if we're selling non-fashion
--- spec/lib/spree/calculator/shipping/ohmyzip_shipping_calculator_spec.rb
+++ spec/lib/spree/calculator/shipping/ohmyzip_shipping_calculator_spec.rb
@@ -1,45 +1,52 @@
 require 'spec_helper'
 
-describe Spree::Calculator::Ohmyzip do
+module Spree
+  module Calculator::Shipping
+    describe Ohmyzip do
 
-  let(:calculator) { described_class.new }
+      let(:calculator) { described_class.new }
 
-  let(:variant1) { double("Variant", 
-    weight: 1, width: 1, depth: 1, height: 1, price: 4) }
-  let(:variant2) { double("Variant", 
-    weight: 2, width: 1, depth: 1, height: 1, price: 6) }
+      let(:variant1) { double("Variant", 
+        weight: 1, width: 1, depth: 1, height: 1, price: 4) }
+      let(:variant2) { double("Variant", 
+        weight: 2, width: 1, depth: 1, height: 1, price: 6) }
 
-  let(:package) { 
-    double(
-      Stock::Package,
-      order: mock_model(Order),
-      contents: [Stock::Package::ContentItem.new(1,variant1, 1)]
-    ) 
-  }
+      let(:package) { 
+        double(
+          Stock::Package,
+          order: mock_model(Order),
+          contents: [Stock::Package::ContentItem.new(1,variant1, 1)]
+        ) 
+      }
 
-  let(:package2) {
-    double(
-      Stock::Package,
-      order: mock_model(Order),
-      contents: [Stock::Package::ContentItem.new(1,variant1, 2),
-        Stock::Package::ContentItem.new(2,variant2, 2)]
-    )
-  }
+      let(:package2) {
+        double(
+          Stock::Package,
+          order: mock_model(Order),
+          contents: [Stock::Package::ContentItem.new(1,variant1, 1),
+            Stock::Package::ContentItem.new(2,variant2, 2)]
+        )
+      }
 
-  it "returns correct description" do
-    expect(calculator.description).to eq 'Ohmyzip Shipping Calculator'
-  end
+      it "returns correct description" do
+        expect(calculator.description).to eq 'Ohmyzip Shipping Calculator'
+      end
 
-  it "correctly calculates shipping cost when weight is 1 lb" do
-    calculator.compute_package(package).should == 8.5
-  end
-  
-  it "correctly calculates shipping cost when weight is 2 lbs" do
-    calculator.compute_package(package2).should == 10.5
-  end
-  
-  it "correctly calculates shipping cost when weight is 10 lbs" do
-    calculator.compute_package(package5).should == 16.5
+      it "correctly calculates weight for a package with multiple items" do
+        calculator.total_weight(package2).should == 3
+      end
+
+      it "correctly calculates shipping cost when weight is 1 lb" do
+        calculator.compute_package(package).should == 8.5
+      end
+      
+      it "correctly calculates shipping cost when weight is 2 lb" do
+        calculator.compute_package(package).should == 10.5
+      end
+
+      it "correctly calculates shipping cost when weight is 3 lbs" do
+        calculator.compute_package(package2).should == 12.5
+      end
+    end
   end
 end
-
