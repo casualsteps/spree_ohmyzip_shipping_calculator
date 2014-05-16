be0f980b217bc6d45c7d0eb282a69e88caf4201d
James Duffy <jamesmartinduffy@gmail.com>
2014-05-16 14:54:11 +0900

    Initial commit

---
+++ lib/spree/calculator/shipping/ohmyzip_shipping_calculator.rb
@@ -0,0 +1,43 @@
+class Spree::Calculator::Ohmyzip < Spree::Calculator
+  preference :default_weight, :decimal, default: 1
+
+  def self.description
+    "Ohmyzip Shipping Calculator"
+  end
+
+  def self.register
+    super
+  end
+
+  def available?(package)
+    true
+  end
+
+  def compute_package(package)
+    content_items = package.contents
+    total_weight = total_weight(content_items)
+    base_price = order_contains_ilbantongwan? ? 7.50 : 6.50
+
+    shipping_cost = total_weight*2 + base_price
+    shipping_cost
+  end
+
+  private
+  def total_weight(contents)
+    weight = 0
+    contents.each do |item|          
+      weight += item.quantity * (item.variant.weight > 0.0 ? item.variant.weight : preferred_default_weight)
+    end
+    
+    weight
+  end
+
+  def order_contains_ilbantongwan?(contents)
+    return false
+    ### TODO: set up the below logic if we're selling non-fashion
+    #contents.each do |item|          
+    ###check each item's category to see if it's not 목록통관
+    #end
+  end
+end
+
---
+++ lib/spree_ohmyzip_shipping_calculator/engine.rb
@@ -0,0 +1,13 @@
+module SpreeOhmyzipShippingCalculator
+  class Engine < Rails::Engine
+    isolate_namespace Spree
+    engine_name 'spree_ohmyzip_shipping_calculator'
+
+    config.autoload_paths += %W({#{config.root}/lib)
+
+    initializer 'spree.register.calculators' do |app|
+      require 'spree/calculator/shipping/ohmyzip_shipping_calculator'
+      app.config.spree.calculators.tax_rates << Spree::Calculator::Ohmyzip
+    end
+  end
+end
