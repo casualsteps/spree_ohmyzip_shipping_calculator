28a3b14a3644197a63c05be79d76ae7ec820830a
James Duffy <jamesmartinduffy@gmail.com>
2014-05-28 16:37:42 +0900

    Modified display method to show KRW. Improved documentation

--- lib/spree/calculator/shipping/ohmyzip_shipping_calculator.rb
+++ lib/spree/calculator/shipping/ohmyzip_shipping_calculator.rb
@@ -10,12 +10,16 @@ class Spree::Calculator::Shipping::Ohmyzip < Spree::ShippingCalculator
     super
   end
 
+
   def local_shipping_amount
     preferred_local_shipping_charge
   end
 
+  # should display in KRW
   def display_local_shipping_amount
-    display_amount = Spree::Money.new(preferred_local_shipping_charge, { currency: "KRW" })
+    presentation_currency = Spree::Config[:presentation_currency] if Spree::Config[:presentation_currency] != nil
+    @rate = if @rate then @rate else Spree::CurrencyRate.find_by(target_currency: presentation_currency) end
+    display_amount = Spree::Money.new(@rate.convert_to_won(preferred_local_shipping_charge).amount, { currency: presentation_currency })
     preferred_local_shipping_charge == 0 ? "기본" : "+ <strong>#{display_amount}</strong>".html_safe
   end
 
@@ -23,15 +27,17 @@ class Spree::Calculator::Shipping::Ohmyzip < Spree::ShippingCalculator
     true
   end
 
+  # computes in USD
   def compute_package(package)
     content_items = package.contents
     total_weight = total_weight(content_items)
     base_price = order_contains_ilbantongwan?(package) ? 7.50 : 6.50
 
-    shipping_cost = total_weight*2 + base_price
+    shipping_cost = total_weight * 2 + base_price
     shipping_cost + preferred_local_shipping_charge
   end
 
+  # computes in USD – does NOT include local shipping upgrade
   def compute_amount(order)
     content_items = order.line_items
     total_weight = total_weight(content_items)
@@ -39,11 +45,6 @@ class Spree::Calculator::Shipping::Ohmyzip < Spree::ShippingCalculator
 
     shipping_cost = total_weight * 2 + base_price
     shipping_cost
-  end
-
-  def display_amount(order)
-    total = compute_amount(order)
-    Spree::Money.new(total, { currency: "KRW" })
   end
 
   def total_weight(contents)
