059bed34f9a0b02a26d08961bc706d3321b4979f
Blaž Hrastnik <blaz.hrast@gmail.com>
2015-03-20 11:52:46 +0900

    First refactoring pass.

--- lib/spree/calculator/shipping/ohmyzip_shipping_calculator.rb
+++ lib/spree/calculator/shipping/ohmyzip_shipping_calculator.rb
@@ -18,39 +18,33 @@ class Spree::Calculator::Shipping::Ohmyzip < Spree::ShippingCalculator
   end
 
   def local_shipping_amount(product_or_order)
-    # get an array of products to iterate on
     products = []
     case product_or_order
-      when Spree::Product
-        products.push product_or_order
-      when Spree::Order
-        product_or_order.line_items.each {|li|
-          for i in 0..li.quantity-1
-            products.push li.product
-          end
-        }
-    end
-
-    return nil if products.empty?
-
-    products_by_merchant = products.group_by { |p| p.merchant }
-    local_shipping_total = 0
-    products_by_merchant.map do |merchant, products|
-      merchant_total = products.sum(&:price).to_f
-      case merchant
-        when "gap", "bananarepublic"
-          local_shipping_total += 7
-        when "footlocker"
-          local_shipping_total += 7.99 + ((products.count-1) * 1.99)
-        when "ssense"
-          # ssense local shipping is free for now. 
-          # Use the code below if they change it later:
-          #
-          #local_shipping_total += 12 if merchant_total <= 200
+    when Spree::Product
+      products.push product_or_order
+    when Spree::Order
+      product_or_order.line_items.each do |li|
+        li.quantity.times { products.push li.product }
       end
     end
 
-    local_shipping_total
+    return 0 if products.empty?
+
+    products.group_by(&:merchant).inject(0) do |total, (merchant, products)|
+      merchant_total = products.sum(&:price).to_f
+      case merchant
+      when "gap", "bananarepublic"
+        total += 7
+      when "footlocker"
+        total += 7.99 + ((products.count-1) * 1.99)
+      when "ssense"
+        # ssense local shipping is free for now.
+        # Use the code below if they change it later:
+        #
+        # total += 12 if merchant_total <= 200
+        total
+      end
+    end
   end
 
   def available?(package)
@@ -59,51 +53,43 @@ class Spree::Calculator::Shipping::Ohmyzip < Spree::ShippingCalculator
 
   # computes in USD
   def compute_package(package)
-    if (package.order.item_count > 1 and preferences[:promotional_shipping_discount]) and (package.order.completed_at.nil? or package.order.completed_at >= Date.parse('2014-11-23'))
+    if (package.order.item_count > 1 && preferences[:promotional_shipping_discount]) && (package.order.completed_at.nil? || package.order.completed_at >= Date.parse('2014-11-23'))
       return local_shipping_amount(package.order)
     end
-    content_items = package.contents
-    total_weight = total_weight(content_items)
-    international_shipping_charge(total_weight) + local_shipping_amount(package.order)
+    international_shipping_charge_for_items(package.contents) + local_shipping_amount(package.order)
   end
 
   # computes in USD
   def compute_amount(order)
-    if (order.item_count > 1 and preferences[:promotional_shipping_discount]) and (order.completed_at.nil? or order.completed_at >= Date.parse('2014-11-23'))
+    if (order.item_count > 1 && preferences[:promotional_shipping_discount]) && (order.completed_at.nil? || order.completed_at >= Date.parse('2014-11-23'))
       return local_shipping_amount(order)
     end
-    content_items = order.line_items
-    total_weight = total_weight(content_items)
-    international_shipping_charge(total_weight) + local_shipping_amount(order)
+    international_shipping_charge_for_items(order.line_items) + local_shipping_amount(order)
   end
 
-  def relaxation_shipping_amount(order)
-    content_items = order.line_items
+  def international_shipping_charge_for_items(content_items)
     total_weight = total_weight(content_items)
     international_shipping_charge(total_weight)
   end
-
-  def _compute_product_weight(product)
-    if product.variants.present?
-      weight = product.variants.maximum(:weight)
-    else
-      weight = product.master.weight
-    end
-    weight = weight > 0.0 ? weight / 100 : preferred_default_weight
-    weight = weight.ceil
+  # TODO: remove after it's removed from master
+  def relaxation_shipping_amount(order)
+    international_shipping_charge_for_items(order.line_items)
   end
 
   def compute_product_amount(product)
-    weight = _compute_product_weight(product)
+    weight = compute_product_weight(product)
     international_shipping_charge(weight) + local_shipping_amount(product)
   end
   
   def show_shipping_charge(location, currency, product)
-    if (location == "local")
+    case location
+    when "local"
       shipping_cost = local_shipping_amount(product)
-    elsif (location == "international")
-      weight = _compute_product_weight(product)
+    when "international"
+      weight = compute_product_weight(product)
       shipping_cost = international_shipping_charge(weight)
+    else
+      raise "Unsupported location value!"
     end
     if (currency == "KRW")
       rate = Spree::CurrencyRate.find_by(target_currency: currency)
@@ -114,10 +100,23 @@ class Spree::Calculator::Shipping::Ohmyzip < Spree::ShippingCalculator
   end
 
   def total_weight(contents)
-    weight = 0
-    contents.each do |item|
-      weight += item.quantity * (item.variant.weight > 0.0 ? item.variant.weight / 100 : preferred_default_weight)
+    contents.inject(0) do |weight, item|
+      weight += item.quantity * default_weight(item.variant.weight)
+    end.ceil
+  end
+
+  private
+
+  def default_weight(weight)
+    weight > 0.0 ? weight / 100 : preferred_default_weight
+  end
+
+  def compute_product_weight(product)
+    weight = if product.variants.present?
+      product.variants.maximum(:weight)
+    else
+      product.master.weight
     end
-    weight.ceil
+    default_weight(weight).ceil
   end
 end
